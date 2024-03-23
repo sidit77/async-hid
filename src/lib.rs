8b22ee659d9898d93a4c3a269fab4db9c217993e
unknown <tarasikpalczynski@gmail.com>
2024-03-22 23:03:17 -0400

    Ordered DeviceInfo trait impls to after DeviceInfo's impl

--- src/lib.rs
+++ src/lib.rs
@@ -33,30 +33,6 @@ pub struct DeviceInfo {
     pub(crate) private_data: BackendPrivateData,
 }
 
-impl Hash for DeviceInfo {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.id.hash(state);
-        self.name.hash(state);
-        self.product_id.hash(state);
-        self.vendor_id.hash(state);
-        self.usage_id.hash(state);
-        self.usage_page.hash(state);
-    }
-}
-
-impl PartialEq for DeviceInfo {
-    fn eq(&self, other: &Self) -> bool {
-        self.id == other.id
-            && self.name == other.name
-            && self.product_id == other.product_id
-            && self.vendor_id == other.vendor_id
-            && self.usage_id == other.usage_id
-            && self.usage_page == other.usage_page
-    }
-}
-
-impl Eq for DeviceInfo {}
-
 impl DeviceInfo {
     /// Enumerates all **accessible** HID devices
     ///
@@ -81,6 +57,30 @@ impl DeviceInfo {
         self.usage_page == usage_page && self.usage_id == usage_id && self.vendor_id == vendor_id && self.product_id == product_id
     }
 }
+
+impl Hash for DeviceInfo {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.id.hash(state);
+        self.name.hash(state);
+        self.product_id.hash(state);
+        self.vendor_id.hash(state);
+        self.usage_id.hash(state);
+        self.usage_page.hash(state);
+    }
+}
+
+impl PartialEq for DeviceInfo {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+            && self.name == other.name
+            && self.product_id == other.product_id
+            && self.vendor_id == other.vendor_id
+            && self.usage_id == other.usage_id
+            && self.usage_page == other.usage_page
+    }
+}
+
+impl Eq for DeviceInfo {}
 
 pub trait SerialNumberExt {
     fn serial_number(&self) -> Option<&str>;
